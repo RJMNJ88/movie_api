b66c60e2afd3be5346f8496e061264418fa00518
RJMNJ88 <90220183+RJMNJ88@users.noreply.github.com>
2021-12-04 13:44:56 -0500

    Update movie list

--- index.js
+++ index.js
@@ -1,86 +1,193 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require('express'),
+    morgan = require('morgan'),
+    bodyParser = require('body-parser'),
+    uuid = require('uuid');
 
 
 const app = express();
+app.use(bodyParser.json());
 
 //Log requests using Morgan
 app.use(morgan('common'));
 
 //Top-ten movie list
-let topMovies = [
+let movies = [
     {
         title: 'The Lord of the Rings: The Fellowship of the Ring',
         genre: ['Fantasy'],
-        releaseYear: 2001 ,
-        director: 'Peter Jackson'
+        released: 2001 ,
+        director: 
+            {
+                name: 'Peter Jackson',
+                born: 'October 31, 1961',
+                country: 'New Zealand'
+            }
     },
     {
         title: 'The Hobbit: An Unexpected Journey',
         genre: ['Fantasy'],
-        releaseYear: 2012,
-        director: 'Peter Jackson'
+        released: 2012,
+        director: 
+            {
+                name: 'Peter Jackson',
+                born: 'October 31, 1961',
+                country: 'New Zealand'
+            }
     },
     {
         title: 'The Girl with the Dragon Tattoo',
         genre: ['Thriller'],
-        releaseYear: 2011,
-        director: 'David Fincher'
+        released: 2011,
+        director: 
+            {
+                name: 'David Fincher',
+                born: 'August 28, 1962',
+                country: 'USA'
+            }
     },
     {
         title: 'Layer Cake',
         genre: ['Crime', 'Thriller'],
-        releaseYear: 2005,
-        director: 'Matthew Vaughn'
+        released: 2005,
+        director: 
+            {
+                name: 'Matthew Vaughn',
+                born: 'March 7, 1971',
+                country: 'England'
+            }
     },
     {
         title: 'Lock, Stock, and Two Smoking Barrels',
         genre: ['Crime', 'Thriller'],
-        releaseYear: 1999,
-        director: 'Guy Ritchie'
+        released: 1999,
+        director: 
+            {
+                name: 'Guy Ritchie',
+                born: 'September 10, 1968',
+                country: 'England'
+            }
     },
     {
         title: 'Angela\'s Ashes',
         genre: ['Drama'],
-        releaseYear: 1999,
-        director: 'Alan Parker'
+        released: 1999,
+        director: 
+            {
+                name: 'Alan Parker',
+                born: 'February 14, 1944',
+                country: 'England'
+            }
     },
     {
         title: 'Casino Royale',
         genre: ['Action', 'Thriller'],
-        releaseYear: 2006,
-        director: 'Martin Campbell'
+        released: 2006,
+        director: 
+            {
+                name: 'Martin Campbell',
+                born: 'October 24, 1943',
+                country: 'New Zealand'
+            }
     },
     {
         title: 'The Best Exotic Marigold Hotel',
         genre: ['Drama'],
-        releaseYear: 2012,
-        director: 'John Madden'
+        released: 2012,
+        director: 
+            {
+                name: 'John Madden',
+                born: 'April 8, 1949',
+                country: 'England'
+            }
     },
     {
         title: 'Like Father, Like Son',
         genre: ['Drama'],
-        releaseYear: 2014,
-        director: 'Hirokazu Koreeda'
+        released: 2014,
+        director: 
+            {
+                name: 'Hirokazu Koreeda',
+                born: 'June 6, 1962',
+                country: 'Japan'
+            }
     },
     {
         title: 'Harry Potter and the Sorcerer\'s Stone',
         genre: ['Fantasy'],
-        releaseYear: 2001,
-        director: 'Chris Columbus'
+        released: 2001,
+        director: 
+            {
+                name: 'Chris Columbus',
+                born: 'September 10, 1958',
+                country: 'USA'
+            }
     }
 ];
 
+// GET Requests
+
+//Gets the list of data about all movies
+app.get('/movies', (req, res) => {
+    res.send('Here is the list of films you requested.');
+});
+
+//Return data: description, genre, director, image URL
+app.get('/movies/:title', (req, res) => {
+    res.send('Here is the description of the film you searched for.');
+});
+
+//Return data about a genre by searching a genre-type
+app.get('/genre', (req, res) => {
+    res.send('Here is a list of films matching that genre.');
+});
+
+//Return data about a director by name
+app.get('/director', (req, res) => {
+    res.send('Here is a list of films from that director.');
+});
+
+//Allow new users to register
+app.post('/users', (req, res) => {
+    res.send('Your account has been succesfully registered.');
+});
+
+//Allow users to update user info
+app.put('/users/:userinfo', (req, res) => {
+    res.send('User info has been successfully updated.');
+});
+
+//Allow users to add a movie to their list of favorites
+app.post('/users/:userinfo/movies/:title', (req, res) => {
+    res.send('New movie has been added to your list of favorites.');
+});
+
+//Allow users to remove a movie from their list of favorites
+app.delete('/users/:userinfo/movies/:title', (req, res) => {
+    res.send('Movie has been deleted from your list of favorites.');
+});
+
+//Allow existing users to de-register
+app.delete('/users/:userinfo', (req, res) => {
+    res.send('User account has been deleted.');
+});
+
+
+//Listen for requests
+app.listen(8080, () => {
+    console.log('Your app is listening on Port 8080.');
+});
+
+
+
+
+// -------------------- OLD CODE ----------------------- //
+/*
 //GET requests
 app.get('/', (req, res) => {
     res.send('Welcome to my Top-Ten Movies Page !');
 });
 
 app.use('/documentation', express.static('public'));
-
-// app.get('/documentation', (req, res) => {
-//     res.sendFile('public/documentation.html', {root:__dirname});
-// });
 
 app.get('/movies', (req, res) => {
     res.json(topMovies);
@@ -91,8 +198,4 @@ app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something went wrong !');
 });
-
-//Listen for requests
-app.listen(8080, () => {
-    console.log('Your app is listening on Port 8080.');
-});
+*/
