b0c9af05d39de81df3498836c61a331847aa8db4
RJMNJ88 <90220183+RJMNJ88@users.noreply.github.com>
2021-12-28 11:18:35 -0500

    GET request -> "/movies" endpoint
    remove passport authentication temporarily

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ app.get('/users', passport.authenticate('jwt', {session: false}), (req, res) => 
 });
 
 //Get a list of all movies
-app.get('/movies', passport.authenticate('jwt', {session: false}), (req, res) => {
+app.get('/movies', (req, res) => {   // passport.authenticate('jwt', {session: false}),
     Movies.find()
     .then((movies) => {
         res.status(201).json(movies);
