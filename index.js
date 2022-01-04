43fa6155308de621955338dc275f5110af6c6b00
RJMNJ88 <90220183+RJMNJ88@users.noreply.github.com>
2022-01-04 13:39:41 -0500

    Authentication
    add passport auth back into "/movies" get request

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ app.get('/users', passport.authenticate('jwt', {session: false}), (req, res) => 
 });
 
 //Get a list of all movies
-app.get('/movies', (req, res) => {   // passport.authenticate('jwt', {session: false}),
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => { 
     Movies.find()
     .then((movies) => {
         res.status(201).json(movies);
